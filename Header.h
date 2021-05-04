e013d5d66eb8e8d3ec0deb2011bbfbab6fa0313a
typicframe <79899886+typicframe@users.noreply.github.com>
2021-05-04 12:22:46 +0800

    Add files via upload

---
+++ Header.h
@@ -0,0 +1,8 @@
+#ifndef header_h
+#define header_h
+
+class header
+{
+	header();
+};
+#endif // !header_h
---
+++ Source.cpp
@@ -0,0 +1,6 @@
+#inclide<iostream>
+using namespace std;
+int main()
+{
+	cout << "holle would" << endl;
+}
